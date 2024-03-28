bd558b88308f90bf37f07cbc2a7f495508fd5142
CainTremBala <160613699+CainTremBala@users.noreply.github.com>
2024-03-28 19:36:16 -0300

    Add files via upload

---
+++ banda/src/script/script.js
@@ -0,0 +1,59 @@
+ScrollReveal().reveal('.img_home', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.descriçao', {
+    origin: 'right',
+    duration: 2000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.album_title', {
+    origin: 'right',
+    duration: 2000,
+    distance: '30%'
+});
+
+ScrollReveal().reveal('.album_cnt', {
+    origin: 'left',
+    duration: 2000,
+    distance: '30%'
+});
+
+ScrollReveal().reveal('.texto_contato', {
+    origin: 'top',
+    duration: 2000,
+    distance: '30%'
+});
+
+ScrollReveal().reveal('.form_01', {
+    origin: 'bottom',
+    duration: 2000,
+    distance: '30%'
+});
+
+ScrollReveal().reveal('.texto_musica', {
+    origin: 'left',
+    duration: 2000,
+    distance: '30%'
+});
+
+ScrollReveal().reveal('.i_musicas', {
+    origin: 'right',
+    duration: 2000,
+    distance: '30%'
+});
+
+ScrollReveal().reveal('.img_turne', {
+    origin: 'bottom',
+    duration: 2000,
+    distance: '30%'
+});
+
+ScrollReveal().reveal('.titte_turne', {
+    origin: 'top',
+    duration: 2000,
+    distance: '30%'
+});
